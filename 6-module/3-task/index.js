7994cb415f05364a6cccd501b3b462d83d417122
Egor <yatskovskiy2001@gmail.com>
2022-05-13 23:44:35 +0300

    Доработана 3 задача ,модуль 6

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,21 +1,63 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+
+  constructor(slides) {
+    this.slides = slides;
+    this.createCarousel(slides);
+  }
+
   get elem() {
     return this._elem;
+  }
+
+  createCarousel(slides) {
+    const carousel = createElement(`
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="../../assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+    
+        <div class="carousel__inner">
+          ${slides.map(slide => this.createSlide(slide))}
+        </div>
+      </div>
+    `);
+
+    const carouselSlides = carousel.querySelectorAll('.carousel__slide');
+
+    this.initBtns(carouselSlides)
+    this.initCarousel(carousel);
+    this._elem = carousel;
+  }
+
+  createSlide (slide) {
+    const slideHTML = createElement(`
+        <div class="carousel__slide" data-id="penang-shrimp">
+          <img src="../../assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button" data-id = ${slide.id}>
+              <img src="../../assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+      `);
+
+      return slideHTML.outerHTML;
   }
 
   initCarousel(carousel) {
     let currentSlide = 0;
     const next = carousel.querySelector(".carousel__arrow_right"),
         prev = carousel.querySelector(".carousel__arrow_left"),
-        inner = carousel.querySelector(".carousel__inner")
+        inner = carousel.querySelector(".carousel__inner");
   
-    const totalSlides = carousel.querySelectorAll(".carousel__slide").length,
-        // В тестах пишет Cannot read properties of null (reading 'offsetWidth') ¯\_(ツ)_/¯, В браузере все норм
-        //slideWidth = document.querySelector(".container").offsetWidth;
-        slideWidth = 500;
-
+    const totalSlides = carousel.querySelectorAll(".carousel__slide").length;
     prev.style.display = "none";
     
     carousel.addEventListener("click", (e) => {
@@ -24,6 +66,8 @@ export default class Carousel {
       let isPrevBtn = !!e.target.closest(".carousel__arrow_left");
   
       if (!isNextBtn && !isPrevBtn) return;
+
+      const slideWidth = carousel.offsetWidth;
   
       if (isNextBtn && currentSlide !== totalSlides-1) {
         currentSlide === totalSlides-2 ? next.style.display = "none" : prev.style.display = "";
@@ -36,66 +80,22 @@ export default class Carousel {
         currentSlide--;
         inner.style.transform = `translateX(-${slideWidth*currentSlide}px)`;
       }
-    })
-  }
-
-  createCarousel(slides) {
-    let carousel = createElement(`
-      <div class="carousel">
-        <div class="carousel__arrow carousel__arrow_right">
-          <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
-        </div>
-        <div class="carousel__arrow carousel__arrow_left">
-          <img src="../../assets/images/icons/angle-left-icon.svg" alt="icon">
-        </div>
-    
-        <div class="carousel__inner">
-
-        </div>
-      </div>
-    `);
-    let inner = carousel.querySelector(".carousel__inner");
-
-
-    for (let i = 0;i < slides.length; i++) {
-      let slide = slides[i];
-      let slideHTML = createElement(`
-        <div class="carousel__slide" data-id="penang-shrimp">
-          <img src="../../assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
-          <div class="carousel__caption">
-            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
-            <div class="carousel__title">${slide.name}</div>
-            <button type="button" class="carousel__button">
-              <img src="../../assets/images/icons/plus-icon.svg" alt="icon">
-            </button>
-          </div>
-        </div>
-      `);
-
-      let btn = slideHTML.querySelector(".carousel__button");
-      this.addCard(btn,slide.id);
-
-      inner.append(slideHTML);
-    }
-
-    this.initCarousel(carousel);
-    this._elem = carousel;
-  }
-
-  addCard(btn,id){
-    btn.addEventListener("click", (e) => {
-      
-      let event = new CustomEvent("product-add", {
-        detail: id,
-        bubbles: true
-      });
-
-      btn.dispatchEvent(event);
     });
   }
 
-  constructor(slides) {
-    this.slides = slides;
-    this.createCarousel(slides);
+  initBtns(slidesHTML) {
+
+    slidesHTML.forEach((slide) => {
+      const btn = slide.querySelector('.carousel__button');
+
+      btn.addEventListener("click", () => {
+        let event = new CustomEvent("product-add", {
+          detail: btn.dataset.id,
+          bubbles: true
+        });
+        btn.dispatchEvent(event);
+      });
+    });
+
   }
 }
