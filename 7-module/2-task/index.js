172500ed281ef2f1d05b818ea1783ddcf75f9bba
Egor <yatskovskiy2001@gmail.com>
2022-05-17 19:08:16 +0300

    Сделаны задания 7 модуля

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,73 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.titleHTML = "";
+    this.bodyHTML = "";
+    this.#render();
+  }
+
+  #elem = null
+
+  #render = () => {
+    this.#createModal();
+    this.#initBtnClose();
+  }
+
+  #createModal = () => {
+    this.#elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+
+            <h3 class="modal__title">${this.titleHTML}</h3>
+
+          </div>
+
+          <div class="modal__body">${this.bodyHTML.outerHTML}</div>
+        </div>
+
+      </div>
+    `);
+  }
+
+  open = () => {
+    document.body.append(this.#elem);
+    document.body.classList.add("is-modal-open");
+    document.addEventListener("keydown", this.#closeESC);
+  }
+
+  setTitle = (title) => {
+    this.titleHTML = title;
+    this.#elem.querySelector(".modal__title").textContent = title;
+  }
+
+  setBody = (node) => {
+    this.bodyHTML = node;
+    this.#elem.querySelector(".modal__body").innerHTML = node.outerHTML;
+  }
+
+  close = () => {
+    this.#elem.remove();
+    document.body.classList.remove("is-modal-open");
+    document.removeEventListener('keydown', this.#closeESC);
+  }
+
+  #closeESC = (e) => {
+    if (e.code === "Escape") {
+      this.close();
+    }
+  }
+
+  #initBtnClose = () => {
+    const closeBtn = this.#elem.querySelector(".modal__close");
+
+    closeBtn.addEventListener('click', () => {
+      this.close();
+    });
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,83 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.#render();
+  }
+
+  get elem () {
+    return this._elem;
+  }
+
+  #render() {
+    const Slider = this.#createSlider();
+    this.$initSlider(Slider);
+    this._elem = Slider;
+  }
+
+  #createSlider = () => {
+    return createElement(`
+    <div class="slider">
+
+      <div class="slider__thumb">
+        <span class="slider__value">0</span>
+      </div>
+
+      <div class="slider__progress"></div>
+
+      <div class="slider__steps">
+        ${this.#createSteps()}
+      </div>
+    </div>
+    `);
+  }
+
+  #createSteps = () => {
+    let steps = "";
+    for (let i = 0; i < this.steps; i++) {
+      steps += `<span class = "${this.value === i ? "slider__step-active" : ""}"></span>`;
+    }
+    return steps;
+  }
+
+  $initSlider = (container) => {
+    const spans = container.querySelectorAll("span");
+    const thumb = container.querySelector(".slider__thumb");
+    const progress = container.querySelector(".slider__progress");
+
+    container.addEventListener("click", (e) => {
+      let width = container.offsetWidth;
+      let x = e.pageX - container.getBoundingClientRect().x;
+
+      const currentStep = Math.round((x * (this.steps - 1)) / width);
+      this.value = currentStep;
+
+
+      thumb.style.left = ((currentStep) / (this.steps - 1)) * 100 + "%";
+      progress.style.width = ((currentStep) / (this.steps - 1)) * 100 + "%";
+
+      this.#setCurrentStep(spans);
+      this.#dispatchEvent(container);
+    });
+  }
+
+  #dispatchEvent = (container) => {
+    container.dispatchEvent(new CustomEvent('slider-change', {
+      detail: this.value,
+      bubbles: true
+    }));
+  }
+
+  #setCurrentStep = (spans) => {
+    spans.forEach((span, index) => {
+      if (span.classList.contains("slider__step-active")) {
+        span.classList.remove("slider__step-active");
+      }
+      if (index === this.value + 1) {
+        span.classList.add("slider__step-active");
+      }
+    });
   }
 }
